bb04f077c702dab30739f01d7493120c8d8e6b30
AmandineRo <mandou_roy@yahoo.fr>
2017-11-13 02:38:20 -0500

    erreur bouton heure/date

--- app/src/main/java/ubiquasif/uqac/betterwithstrangers/HostActivity.java
+++ app/src/main/java/ubiquasif/uqac/betterwithstrangers/HostActivity.java
@@ -1,18 +1,26 @@
 package ubiquasif.uqac.betterwithstrangers;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
+import ubiquasif.uqac.betterwithstrangers.Fragments.CreateEventFragment;
 import ubiquasif.uqac.betterwithstrangers.Helpers.Helper_NavigationBottomBar;
 
-public class HostActivity extends AppCompatActivity {
+public class HostActivity extends AppCompatActivity
+        implements CreateEventFragment.OnFragmentInteractionListener{
 
     private TextView mTextMessage;
+
+    private Fragment createEventFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -22,6 +30,9 @@ public class HostActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_host:
                     mTextMessage.setText(R.string.title_host);
+                    mTextMessage.setVisibility(View.GONE);
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_container, createEventFragment).commit();
+
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
@@ -48,12 +59,23 @@ public class HostActivity extends AppCompatActivity {
         setContentView(R.layout.activity_host);
 
         mTextMessage = findViewById(R.id.message);
-        
+
+        // Set content view AFTER ABOVE sequence (to avoid crash)
+        this.setContentView(R.layout.activity_host);
+
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         // Fix navigation bar strange behavior
         Helper_NavigationBottomBar.disableShiftMode(navigation);
+
+        createEventFragment = CreateEventFragment.newInstance(null,null);
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        Log.println(Log.DEBUG,"debug", "Fragment interaction detected");
     }
 
 }
