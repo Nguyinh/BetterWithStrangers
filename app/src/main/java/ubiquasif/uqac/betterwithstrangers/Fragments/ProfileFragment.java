8c5310fee5e9463c35a53152cf56014f21b18133
Florent Dupont <florent.dupont@edu.esiee.fr>
2017-11-20 23:10:08 +0100

    Added some parts for ProfileFragment
    - Added minibiography
    - Added NachosTextView for preferences
    - Added rating view for Host and Guest views
    TODO : Handle NachosTextView

--- app/src/main/java/ubiquasif/uqac/betterwithstrangers/Fragments/ProfileFragment.java
+++ app/src/main/java/ubiquasif/uqac/betterwithstrangers/Fragments/ProfileFragment.java
@@ -7,9 +7,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.text.Editable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -19,7 +23,18 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.hootsuite.nachos.ChipConfiguration;
+import com.hootsuite.nachos.NachoTextView;
+import com.hootsuite.nachos.chip.Chip;
+import com.hootsuite.nachos.chip.ChipSpan;
+import com.hootsuite.nachos.chip.ChipSpanChipCreator;
+import com.hootsuite.nachos.terminator.ChipTerminatorHandler;
+import com.hootsuite.nachos.tokenizer.ChipTokenizer;
+import com.hootsuite.nachos.tokenizer.SpanChipTokenizer;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import ubiquasif.uqac.betterwithstrangers.FirstConnectionActivity;
 import ubiquasif.uqac.betterwithstrangers.R;
@@ -36,6 +51,11 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+
+    private NachoTextView tags;
+
+    private String[] suggestions = new String[]{"Tortilla Chips", "Melted Cheese", "Salsa", "Guacamole", "Mexico", "Jalapeno"};
+    private List<String> savedPrefs = new ArrayList<String>();
 
     public ProfileFragment() {
     }
@@ -73,6 +93,9 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
         Button signOutButton = view.findViewById(R.id.sign_out_button);
         signOutButton.setOnClickListener(this);
 
+        Button modifyButton = view.findViewById(R.id.modify_button);
+        modifyButton.setOnClickListener(this);
+
         TextView nameView = view.findViewById(R.id.display_name);
         ImageView photoView = view.findViewById(R.id.profile_photo);
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
@@ -86,6 +109,29 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
             if (photoUrl != null)
                 Picasso.with(getContext()).load(photoUrl).into(photoView);
         }
+
+        tags = view.findViewById(R.id.preferences);
+        tags.addChipTerminator(' ', ChipTerminatorHandler.BEHAVIOR_CHIPIFY_TO_TERMINATOR);
+        tags.addChipTerminator('\n', ChipTerminatorHandler.BEHAVIOR_CHIPIFY_TO_TERMINATOR);
+        tags.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean focus) {
+                if (!focus) {
+                    tags.chipifyAllUnterminatedTokens();
+                }
+            }
+        });
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_dropdown_item_1line, suggestions);
+        tags.setAdapter(adapter);
+
+        for (String word : savedPrefs) {
+            Log.d("Nachos", word);
+            //tags.getChipTokenizer().applyConfiguration(tags.getEditableText(), new ChipConfiguration());
+        }
+
+        //tags.setTextWithChips();
+        //tags.performValidation();
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -126,6 +172,17 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
                         }
                     });
         }
+    }
+
+    @Override
+    public void onStop()
+    {
+        for(Chip chip : tags.getAllChips()) {
+            savedPrefs.add(chip.getText().toString());
+            tags.getChipTokenizer().deleteChip(chip, tags.getEditableText());
+        }
+
+        super.onStop();
     }
 
     public interface OnFragmentInteractionListener {
